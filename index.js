fadf30648abc4e529bfa52c4a3794e6c94c9245b
Your NameTheMossBeard <jessew529@gmail.com>
2020-01-07 12:36:01 -0800

    Added function to populate userinfo table

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const app = express()
 
 dotenv.config()
 
+let AWS = require("aws-sdk")
+/*add your accessKey and secrectAccessKey, please don't go crazy adding stuff to dynamoDB, don't want to get charged*/
+let awsConfig = {
+    "region": "us-west-2",
+    "endpoint": "http://dynamodb.us-west-2.amazonaws.com",
+    "accessKeyId": "AKIAJZCQEGVYJ7S7G6XQ", "secretAccessKey": "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"
+}
+
+/* this populates the dynamoDB table with user info */
+AWS.config.update(awsConfig);
+let docClient = new AWS.DynamoDB.DocumentClient();
+let addUser = function () {
+    //the real user info would go here
+    let input = {
+        "username": "usernametest", "password": "1234567898", "email": "testme@gmail.com", "firstname": "Bill", "lastname": "Nye"
+    }
+    let params = {
+        TableName: "userInfo",
+        Item:  input
+    }
+    docClient.put(params, function (err, data) {
+        if (err) {
+            console.log("users::addUser::error - " + JSON.stringify(err, null, 2));                      
+        } else {
+            console.log("users::addUser::success" );                      
+        }
+    })
+}
+//commented this out so I dont keep adding stuff to dynamoDB
+//addUser();
+
 //Import routes
 const stockRoutes = require('./routes/stocks')
 const userRoutes = require('./routes/users')
