e1ddbe55269d76fa8131b2d81ae6d623bbdad04f
mengistristen <tristen.mengis@gmail.com>
2020-01-09 15:31:55 -0800

    Added token authentication, token refresh, and documentation to users.js

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,15 @@ const AWS = require('aws-sdk')
 const cognito = new AWS.CognitoIdentityServiceProvider()
 
 /*
+    Route: '/users
+    Method: POST
     Purpose: This route is used to create
         a new user
+    Body: {
+        password - user's password,
+        username - user's username,
+        email - user's email
+    }
 */
 router.post('/', (req, res) => {
     const params = {
@@ -24,6 +31,18 @@ router.post('/', (req, res) => {
     })
 })
 
+/*
+    Route: /users/:username/verification
+    Method: PUT
+    Purpose: This route is used to verify a new
+        user's email.
+    Parameters: 
+        username - String
+    Body: {
+        confirmation_code - the confirmation code
+            sent to the user's email address,
+    }
+*/
 router.put('/:username/verification', (req, res) => {
     const params = {
         ClientId: process.env.COGNITO_CLIENT_ID,
@@ -38,12 +57,107 @@ router.put('/:username/verification', (req, res) => {
 })
 
 /*
+    Route: /users/token
+    Method: POST
+    Purpose: This route is used to log in the
+        user and retrieve a new access token.
+    Body: {
+        username - user's username,
+        password - user's password
+    }
+    Response: {
+        access_token - token for getting user information
+        refresh_token - token for updating the access token
+    }
+*/
+router.post('/token', (req, res) => {
+    const params = {
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        ClientId: process.env.COGNITO_CLIENT_ID,
+        AuthParameters: {
+            'USERNAME': req.body.username,
+            'PASSWORD': req.body.password
+        }
+    }
+
+    cognito.initiateAuth(params, (err, data) => {
+        const { AccessToken, RefreshToken } = data
+
+        if (err) res.send(err)
+        else res.send({access_token: AccessToken, refresh_token: RefreshToken})
+    })
+})
+
+/*
+    Route: /users/token
+    Method: PUT
+    Purpose: This route is used to update your
+        access token
+    Body: {
+        refresh_token - refresh token returned 
+            from auth flow
+    }
+    Response: {
+        access_token - new access token for getting
+            user information
+    }
+*/
+router.put('/token', (req, res) => {
+    const params = {
+        AuthFlow: 'REFRESH_TOKEN_AUTH',
+        ClientId: process.env.COGNITO_CLIENT_ID,
+        AuthParameters: {
+            'REFRESH_TOKEN': req.body.refresh_token
+        }
+    }
+
+    cognito.initiateAuth(params, (err, data) => {
+        if (err) res.send(err)
+        else res.send({access_token: data.AccessToken})
+    })
+})
+
+/*
+    Route: /users/:username
+    Method: GET
+    Purpose: This route is used to get the 
+        information about the user.
+    Parameters:
+        username - user's username
+    Body: {
+        token - access token returned from 
+            auth flow
+    }
+    Response: {
+        "Username" - user's username,
+        "UserAttributes" - attributes like the email
+            and email verification status
+    }
+*/
+router.get('/:username', (req, res) => {
+    const params = {
+        AccessToken: req.body.access_token
+    }
+
+    cognito.getUser(params, (err, data) => {
+        if (err) res.send(err)
+        else res.send(data)
+    })
+})
+
+/*
+    Route: /users/:username/username
+    Method: PUT
     Purpose: This route is used to update
         a user's username
+    Parameters:
+        username - user's username
 */
 router.put('/:username/username', (req, res) => {})
 
 /*
+    Route: /users/:username/password
+    Method: PUT
     Purpose: This route is used to update
         a user's password
 */
