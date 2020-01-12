da1ca0f2dfd5a3303e6a62c5e76557b9fccdc01f
Levi Leuthold <leutholdlevi@gmail.com>
2020-01-12 10:49:21 -0800

    Working on call to API for stock market open/closed information in stock.js.

--- routes/stocks.js
+++ routes/stocks.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 const axios = require('axios')
 const dynamoClient = require('../dynamoClient')
+const openMarket = require('https')
+
+/* stock market hours request options */
+const options = {
+    hostname: 'financialmodelingprep.com',
+    port: 443,
+    path: '/api/is-the-market-open?datatype=json',
+    method: 'GET'
+  }
 
 router.get('/', (req, res) => {
     axios.get('https://financialmodelingprep.com/api/v3/stock/real-time-price')
@@ -20,5 +29,21 @@ router.get('/:symbol', (req, res) => {
         else res.json(data.Item)
     })
 })
+
+/* Returns data on whether the stock market is open */
+router.get('/', (req, res) => {
+    const req = openMarket.request(options, (res) => {
+        res.on('data', (d) => {
+          process.stdout.write(d)
+        })
+      })
+      
+      req.on('error', (error) => {
+        console.error(error)
+      })
+      
+      req.end()
+})
+
 
 module.exports = router
