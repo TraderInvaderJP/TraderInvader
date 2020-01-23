dc9ff6375139ff9a57a0b0e056462ab8880a7ba7
mengistristen <tristen.mengis@gmail.com>
2020-01-22 20:08:15 -0800

    Made routes for adding users to games and buying stocks

--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const dynamoClient = require('../dynamoClient')
 /*
     Purpose: This route is used to get all games
         for a specific user
@@ -11,7 +11,14 @@ router.get('/:userid', (req, res) => {})
         for a specific user in a specific game
 */
 router.get('/:gameid/portfolios/:userid', (req, res) => {
-
+    const params = {
+        TableName: 'Portfolio',
+        Item: {
+            username: req.params.userid,
+            gameid: req.params.gameid,
+            stocks: []
+        }
+    }
 })
 
 /*
@@ -24,19 +31,56 @@ router.get('/:gameid/portfolios/:userid/players', (req, res) => {})
     Purpose: This route is used to create a new
         game
 */
-router.post('/', (req, res) => {})
+router.post('/', (req, res) => {
+    
+})
 
 /*
     Purpose: This route is used to add a user 
         to a game
 */
-router.put('/:gameid/users/:userid', (req, res) => {})
+router.put('/:gameid/users/:userid', (req, res) => {
+    const params = {
+        TableName: 'Portfolio',
+        Item: {
+            username: req.params.userid,
+            gameid: req.params.gameid,
+            stocks: []
+        }
+    }
+
+    dynamoClient.put(params, function(err, data) {
+        if (err) res.send(err);
+        else res.send(data)
+    })
+})
 
 /*
     Purpose: This route is used to buy a stock for a 
         specific user's portfolio in a specific game
 */
-router.put('/:gameid/portfolios/:userid/buy', (req, res) => {})
+router.put('/:gameid/portfolios/:userid/buy', (req, res) => {
+    const params = {
+        TableName: 'Portfolio',
+        Key: {
+            username: req.params.userid,
+            gameid: req.params.gameid
+        },
+        UpdateExpression: "SET stocks = list_append(stocks, :i)",
+        ExpressionAttributeValues: {
+            ':i': [{
+                symbol: req.body.symbol,
+                amount: req.body.amount
+            }]
+        },
+        ReturnValues: "UPDATED_NEW"
+    }
+
+    dynamoClient.update(params, (err, data) => {
+        if(err) res.send(err)
+        else res.send(data)
+    })
+})
 
 /*
     Purpose: This route is used to sell a stock from 
