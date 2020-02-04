1088b00c7a358769bc30210e8e4e0239bc1e6c6d
Levi Leuthold <leutholdlevi@gmail.com>
2020-02-04 11:13:48 -0800

    Stubbed out the statistic routes for player stats

--- routes/statistics.js
+++ routes/statistics.js
@@ -12,4 +12,48 @@ router.get('/:userid', (req, res) => {})
 */
 router.get('/:userid/friends', (req, res) => {})
 
+/*
+    Purpose: This route is used to add a 
+        game win to a players number of games won stat
+*/
+router.get('/:userid', (req, res) => {})
+
+/*
+    Purpose: This route is used to add a 
+        game loss to a players number of games lost stats
+*/
+router.get('/:userid', (req, res) => {})
+
+/*
+    Purpose: This route is used to add a 
+        game win to a players win streak stat
+*/
+router.get('/:userid', (req, res) => {})
+
+/*
+    Purpose: This route is used to reset 
+        a players win streak stat
+*/
+router.get('/:userid', (req, res) => {})
+
+/*
+    Purpose: This route is used to add a 
+        game loss to a players loss streak stat
+*/
+router.get('/:userid', (req, res) => {})
+
+/*
+    Purpose: This route is used to reset 
+        a players loss streak stat
+*/
+router.get('/:userid', (req, res) => {})
+
+/*
+    Purpose: This route is used to add a 
+        win to a players daily challenge wins stat
+*/
+router.get('/:userid', (req, res) => {})
+
+/* TODO: Add achievement routes to backend: */
+
 module.exports = router
