3295cbfcf9c1bd4a40197100c0eb356d9df7216f
Levi Leuthold <leutholdlevi@gmail.com>
2020-02-19 22:02:51 -0800

    Asynch code being tested for the lambda function execute code

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const dynamoClient = require('../dynamoClient')
+const axios = require('axios') //temp for testing
 
 /*
     Route: /statistics/users/:userid/games
@@ -220,5 +221,148 @@ router.put('/:userid/achievements', (req, res) => {
         }
     })
 })
+
+/*
+   TEMPORARY FOR TESTING
+*/
+
+var something;
+router.put('/:GameID/Tester', (req, res) => {
+    const params = {
+        TableName: 'Games',
+        Key: {
+            GameID: req.params.GameID
+        }
+    }
+
+    dynamoClient.get(params, async (err, data) => {
+        if (err) res.send(err);
+        else {
+            var userArray = data.Item.data.users;
+            console.log(userArray);
+            var userPortfolioArray = [];
+            var moneyMadeByEachUser = [];
+
+            for (let user of userArray)
+            {
+                var someUser = await getUsersPortfolioValues(user, data.Item.GameID);
+                userPortfolioArray.push(someUser);
+            }
+
+            //console.log(userPortfolioArray);
+
+            for (let userPortfolio of userPortfolioArray)
+            {
+                var value = userPortfolio.Item.wallet;
+                
+                for (var stockSymbol in userPortfolio.Item.stocks) {
+                    var stockValue = await getStockValue(stockSymbol);
+                    value = value + stockValue;
+                }
+                moneyMadeByEachUser.push(value);
+                console.log("EXECUTION");
+            }
+
+            for (var step = 0; step < moneyMadeByEachUser.length; step++)
+            {
+                if (moneyMadeByEachUser[step] < userPortfolioArray[step].Item.wallet)
+                {
+                    moneyMadeByEachUser[step] = moneyMadeByEachUser[step] + userPortfolioArray[step].Item.wallet;
+                }
+            }
+
+            console.log(moneyMadeByEachUser);
+
+            //console.log(moneyMadeForEachUserArray[0].Item.stocks)
+
+
+
+            // const params2 = {
+            //     TableName: 'Portfolio',
+            //     IndexName: 'gameid-index',
+            //     KeyConditionExpression: 'gameid = :gameid',
+            //     ExpressionAttributeValues: {
+            //         ':gameid': req.params.GameID
+            //     },
+            //     ProjectionExpression: 'stocks, username, wallet'
+            // }
+
+            // dynamoClient.query(params2, (err, data) => {
+            //     if (err) console.log("ERROR259");
+            //     else 
+            //     {
+            //         var userPortfolioValues = [];
+            //         for (let step = 0; step < data.Count; step++)
+            //         {
+            //             var value = data.Items[step].wallet;
+
+            //             for (var stockSymbol in data.Items[step].stocks)
+            //             {
+            //                 console.log(data.Items[step].stocks[stockSymbol]);
+                                
+            //                 var stockSymbolPrice;
+            //                 var strArray = [];
+                            
+            //                 vaxios.get('https://financialmodelingprep.com/api/v3/stock/real-time-price/' + stockSymbol)
+                            
+                                
+
+            //                 userPortfolioValues.push(200);
+                            
+            //             }
+
+            //         }
+            //         console.log(userPortfolioValues);
+            //     }
+            // })
+
+
+
+
+            res.send(data)
+        }
+    })
+
+
+})
+
+
+/*
+    Return the value of a users portfolio
+*/
+async function getUsersPortfolioValues(username, gameID) {
+    const params = {
+        TableName: 'Portfolio',
+        Key: {
+            username: username,
+            gameid: gameID
+        }
+    }
+
+    var data = await dynamoClient.get(params, async (err, data) => {
+        const error = err;
+        if (err) console.log("error");
+        else {
+            // var userValue = 0;
+            // userValue = userValue + data.Item.wallet;
+
+            // for (var stockSymbol in data.Item.stocks) {
+            //     var stockValue = await getStockValue(stockSymbol);
+            //     userValue = userValue + stockValue;
+            // }
+            
+            return data;
+        }
+    }).promise()
+
+    return data;
+}
+
+async function getStockValue(stock) {
+    let value = await axios.get('https://financialmodelingprep.com/api/v3/stock/real-time-price/' + stock);
+    return value.data.price;
+}
+
+
 
 module.exports = router
